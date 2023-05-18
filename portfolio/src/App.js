20319ee2d6767e944dbc2478b5972de6ed4ea173
sahajsingh <ssa365@sfu.ca>
2023-05-18 02:42:03 -0700

    Added animated background

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,17 @@
+// eslint-disable-next-line
+import { useCallback, useState } from 'react'
 import React from 'react';
 import { Document, Page } from 'react-pdf';
 import './App.css';
+import Particles from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
 
 function App() {
+  // eslint-disable-next-line
+  const init = useCallback(async (engine) => {
+    await loadFull(engine)
+  })
+  
   const [loading, setLoading] = React.useState(true);
 
   function onDocumentLoadSuccess() {
@@ -11,6 +20,116 @@ function App() {
 
   return (
     <div className="App">
+      <Particles options={{
+        "particles": {
+          "number": {
+            "value": 100,
+            "density": {
+              "enable": true,
+              "value_area": 800
+            }
+          },
+          "color": {
+            "value": "#b362ffff"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#000000"
+            },
+            "polygon": {
+              "nb_sides": 6
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#FFFFFF",
+            "opacity": 0.4,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "bubble"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "repulse"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 7,
+              "duration": 3,
+              "opacity": 1,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true
+      }} init={init}/>
       <div className="App-header">
         <h1 className="typing-animation">Welcome! I'm Sahaj Singh.</h1>
         <div className="button-container">
